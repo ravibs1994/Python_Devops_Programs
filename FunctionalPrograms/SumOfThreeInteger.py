a2f613ba0a32111e0739a5daca8fed559a4d8220
unknown <ravibs1994@gmail.com>
2020-11-18 15:12:05 +0530

    Refactor code:Comment and Header Added

--- FunctionalPrograms/SumOfThreeInteger.py
+++ FunctionalPrograms/SumOfThreeInteger.py
@@ -5,28 +5,32 @@
   * Package:Functional Programs
   * Statement:Sum of three Integer adds to ZERO
 """
+
+
 class SumOfThreeDigit:
-  size=0
+  size = 0
 
-  """Initalization Of Data Members in Constructor"""
-  def __init__(self,size):
-    self.size=size
+  def __init__(self, size):
+    """Initalization Of Data Members in Constructor"""
 
+    self.size = size
+
+  def CalculateSumOfThreeDigitisEqulastoZero(self, array):
     """Operation:Calculate Sum of Three Integers equals to zero From Array and Print That elements
-      return: No return Value 
+          return: No return Value
     """
-  def CalculateSumOfThreeDigitisEqulastoZero(self,array):
-      length = len(array)
-      for i in range(length - 2):
-        for j in range(length - 1):
-          for k in range(length):
-            if array[i] + array[j] + array[k] == 0:
-              print(str(array[i]) + " + " + str(array[j]) + " + " + str(array[k]) + " = 0")
+    length = len(array)
+    for i in range(length - 2):
+      for j in range(length - 1):
+        for k in range(length):
+          if array[i] + array[j] + array[k] == 0:
+            print(str(array[i]) + " + " + str(array[j]) + " + " + str(array[k]) + " = 0")
 
-  """Operation:Create Array as Per Given size
-     :return:Retun the Array 
-  """
   def createArray(self):
+
+    """Operation:Create Array as Per Given size
+       :return:Retun the Array
+    """
     array = []
     for index in range(self.size):
       while True:
@@ -37,18 +41,18 @@ class SumOfThreeDigit:
           print("Opps' You Entered Wrong input plz Enter Digit""")
         break
     return array
-
-"""Main Method 
-   Operation:Create Class Object , Taking User Inputs And Call Class Methods
-"""
 if __name__ == "__main__":
+
+  """Main Method 
+     Operation:Create Class Object , Taking User Inputs And Call Class Methods
+  """
   while True:
+    """ Exception Handling """
     try:
-      size=int(input("Enter The Size Of Array "))
-      object1=SumOfThreeDigit(size)
+      size = int(input("Enter The Size Of Array "))
+      object1 = SumOfThreeDigit(size)
       elementArray = object1.createArray()
       object1.CalculateSumOfThreeDigitisEqulastoZero(elementArray)
       break
     except ValueError:
       print("Opps You enter  wrong input plz enter digits ")
-
