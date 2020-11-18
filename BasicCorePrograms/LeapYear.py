8921b2e8d37c44c5789b45eaee7d2313001c63f2
unknown <ravibs1994@gmail.com>
2020-11-18 15:49:44 +0530

    Refactor code:Comment and Header Added

--- BasicCorePrograms/LeapYear.py
+++ BasicCorePrograms/LeapYear.py
@@ -1,17 +1,19 @@
 """
-   * author - Ravindra Suryawanshi
-   * date - 13/11/2020
-   * time - 00.24
-   * package - Basic core programs
-   * Title -Find Leap Year
+   * Author :Ravindra
+   * Date - 13/11/2020
+   * Time - 00.24
+   * Package - BasicCoreprograms
+   * Statement -Find Leap Year
 """
 class LeapYear:
 
-  # method definition
-
   def findLeapYear(year):
-  # check Year Value Is Equal to 4 digit or not
+    """Method definition
+       Operation: Find leap year from given input year
+       :return:Does not return any value
+     """
 
+  # check Year Value Is Equal to 4 digit or not
     if len(str(year)) == 4:
     # Find Leap Year that is divisible by 4 and 400 not by 100
 
@@ -21,9 +23,9 @@ class LeapYear:
         print(str(year) + " is not Leap year")
     else:
       print("You Entered Invalid Year ")
-# main method call
 
 if __name__ == "__main__":
+  """ main method call"""
   yearValue = int(input("Enter Year: "))
   # method call
   LeapYear.findLeapYear(yearValue)
