d73ab7baf05822463dd66932efe460ba83c17bd1
unknown <ravibs1994@gmail.com>
2020-11-18 15:50:54 +0530

    Refactor code:Comment and Header Added

--- BasicCorePrograms/PowerofTwo.py
+++ BasicCorePrograms/PowerofTwo.py
@@ -1,7 +1,16 @@
+"""
+  * Author :Ravindra
+  * Date   :14-11-2020
+  * Time   :15:30
+  * Package:BasicCorePrograms
+  * Statement:prints a table of the powers of 2 that are less than or equal to 2^N.
+"""
 class PowerOftwo:
-  #method definition
 
    def calculatePowerOfTwo(number):
+    """Method definition
+      Operation:Calculate power of two of given Number
+    """
     # checking number is between0 to 31
     if (0 <= number < 31 ):
       for num in range (number+1):
@@ -10,9 +19,9 @@ class PowerOftwo:
     else:
       print("Entered Number Should be 0 to 30 only")
     return
-#Main method call
 
 if __name__ == "__main__":
+  """Main Mehod """
   numberValue=int (input(" Enter The Number "))
   # Method call
   PowerOftwo.calculatePowerOfTwo(numberValue)
